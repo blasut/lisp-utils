7cc9206c6a95f5631d0b0aea4bbbf774a48dde4e
blasut <blasut937@gmail.com>
2017-03-23 21:08:01 +0100

    play around with thread-it and if-let

--- play.lisp
+++ play.lisp
@@ -7,26 +7,47 @@
   (lambda (&rest more-args)
     (apply func (append args more-args))))
 
-(defun thread-it (&rest fns)
-  "thread-it can be used to concat curried functioncalls into one lambda"
+(defun exec-thread-it (&rest fns)
+  "exec-thread-it can be used to concat curried functioncalls into one lambda"
   (lambda (initial)
     (reduce (lambda (acc f)
               (funcall f acc)) fns :initial-value initial)))
 
-(funcall (thread-it (curry #'+ 1)
-                    (curry #'+ 2))
+(funcall (exec-thread-it (curry #'+ 1)
+           (curry #'+ 2))
          2)
 
-(thread-it (curry #'+ 1)
-           (curry #'+ 2)
-           (curry #'- 2))
+(exec-thread-it (curry #'+ 1)
+  (curry #'+ 2)
+  (curry #'- 2))
 
-(funcall (thread-it (curry #'+ 1)
+(funcall (exec-thread-it (curry #'+ 1)
            (curry #'+ 2)
            (curry #'- 2)) 0)
 
 
-(defmacro exec-thread-it (start &body fns)
-  `(funcall (thread-it ,@fns) ,start))
+(defmacro thread-it (start &body fns)
+  `(funcall (exec-thread-it ,@fns) ,start))
 
-(exec-thread-it 0 (curry #'+ 1) (curry #'+ 1))
+(thread-it 0
+           (curry #'+ 1)
+           (curry #'+ 1))
+
+(destructuring-bind (x (y1 y2) z) (list 1 (list 2 20) 3)
+  (list :x x :y1 y1 :y2 y2 :z z))
+
+;; is this good syntax? not the same as let, but we can also only bind one var...
+
+(let ((x 5))
+  (print x))
+
+(if-let (x 5)
+  (print x))
+
+(if-let (x nil)
+  (print x))
+
+(if-let (x (> 5 0))
+  (print x)
+  (print "lol"))
+
